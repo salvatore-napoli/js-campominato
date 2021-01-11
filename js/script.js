c6cb7da9a4056af8351a75318387e42e7a5591ed
Salvatore Napoli <snapoli1998@gmail.com>
2021-01-11 03:14:32 +0100

    Prompt scelta utente

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function uniqueNumbersGen(arr, count, min, max) {
+function uniqueNumbersGen(arr, count, min, max, arr2) {
   while(arr.length < count) {
     var randomNumber = Math.floor(Math.random() * (max + 1 - min) + min);
     if (!arr.includes(randomNumber)) {
@@ -7,7 +7,23 @@ function uniqueNumbersGen(arr, count, min, max) {
   }
 }
 
-var randomNumbers = [];
-uniqueNumbersGen(randomNumbers, 16, 1, 100)
+function userNumbers(arr, count, min, max) {
+  while(arr.length < max - count) {
+    var numberPick = parseInt(prompt('Scegli un numero compreso tra ' + min + ' e ' + max));
+    if (numberPick < 1 || numberPick > 100) {
+      alert('Il numero inserito non è valido');
+    } else if (isNaN(numberPick)) {
+      alert('Non hai inserito un numero');
+    } else if (arr.includes(numberPick)) {
+      alert('Hai già inserito questo numero!');
+    } else {
+      arr.push(numberPick);
+    }
+  }
+}
 
+var randomNumbers = [];
+var pickedNumbers = [];
+uniqueNumbersGen(randomNumbers, 16, 1, 100);
+userNumbers(pickedNumbers, 16, 1, 100);
 console.log(randomNumbers);
